b0d0f8bd3d18165c7a0acded7884e12878230aa5
Dominic Joseph <dominic@nectarom.com>
2015-02-26 00:50:17 -0600

    Add Alfred chat bubble.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('title', 'Dominic Joseph');
-app.use(express.static(__dirname));
+app.use('/', express.static(__dirname));
+app.use('/alfred', express.static(__dirname + '/alfred.html'));
 
 app.listen(process.env.PORT);
