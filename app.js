52ef6148892528edde52a1b8ff60780f9917abac
Dominic Joseph <dtpjoseph@gmail.com>
2015-04-12 10:31:32 -0500

    Add more background images and whatnot.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express();
+var num = 0;
 
 app.set('title', 'Dominic Joseph');
 app.use('/', express.static(__dirname));
 app.use('/alfred', express.static(__dirname + '/alfred.html'));
+app.use('/bkgd', function (req, res, next) {
+    res.sendFile(__dirname + '/views/img/bkgds/bkdg' + (num % 4 + 1) + '.jpg');
+    num++;
+});
 
 app.listen(process.env.PORT);
